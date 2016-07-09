91a6382a46be0454d77eba3c96065c0a0f590e4b
James Brown <github@mesbrown.co.uk>
2016-07-09 14:22:48 +0100

    fix merge conflict

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 
 const request = require( 'request' )
 const url = 'https://my.pgp-hms.org/public_genetic_data'
 
 const cheerio = require('cheerio')
     $ = cheerio.load('<h2 class="title">Hello world</h2>');
-
-$('h2.title').text('Hello there!');
-$('h2').addClass('welcome');
-
-$.html();
 
 
 request( url, ( err, res ) => {
@@ -22,9 +16,3 @@ function scrape( res ) {
   $ = cheerio.load( res )
   console.log( $('[data-summarize-as="participant"]').text() )
 }
-=======
-console.log('billy gene!');
-var $ = require("cheerio");
-var request = require("request");
-console.log($);
->>>>>>> 5867c9c32e17845993dbb3b2a78209d3ba89adcf

2480bb8c541acb53a6751839195fc1b3dccd03fe
Clarkie <andrew.t.clarke@gmail.com>
2016-07-09 16:15:15 +0100

    wip trumpet

--- trumpet.js
+++ trumpet.js
@@ -1,4 +1,5 @@
 var trumpet = require('trumpet');
+var through2 = require('through2');
 
 var request = require('request');
 var htmlparser = require("htmlparser2");
@@ -8,10 +9,51 @@ var urlString = 'https://my.pgp-hms.org/public_genetic_data?utf8=%E2%9C%93&data_
 var requestStream = request(urlString);
 
 
-var trumpet = trumpet();
+var t = trumpet({objectMode:true});
 
-trumpet.selectAll('tr', function (span) {
-    span.createReadStream().pipe(process.stdout);
+t.selectAll('tr', function (tableRow) {
+    // var trumpet2 = trumpet();
+    // trumpet2.selectAll('td', function (cell) {
+    //   console.log('CELL', cell);
+    // });
+
+    var str = '';
+    var finished = false;
+
+    console.log(arguments);
+
+    tableRow.createReadStream()
+      .pipe(process.stdout)
+      .pipe(
+        through2.obj(
+        function (chunk, enc, cb) {
+          // console.log('-----------------');
+          // console.log(chunk);
+          // this.push("{asdfasdf:'asdf'}")
+
+          str += chunk.toString();
+          cb()
+          if (finished) {
+            this.push(str);
+            console.log('-------------');
+            console.log(str);
+          }
+        }
+      ))
+      .on('end', function () {
+        // this.push(str);
+        finished = true;
+      })
+      .pipe(process.stdout);
 });
 
-requestStream.pipe(trumpet);
+requestStream
+.pipe(t)
+.pipe(through2.obj(
+  function (chunk, enc, cb) {
+    // console.log(chunk)
+    cb();
+  })
+);
+
+

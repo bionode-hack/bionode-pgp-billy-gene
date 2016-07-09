ed67914e423d1f9e67bbb041f82ffe1c062bf68d
James Brown <github@mesbrown.co.uk>
2016-07-09 14:40:20 +0100

    read from file

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-
-const request = require( 'request' )
-const url = 'https://my.pgp-hms.org/public_genetic_data'
-
-const cheerio = require('cheerio')
-    $ = cheerio.load('<h2 class="title">Hello world</h2>');
-
-
-request( url, ( err, res ) => {
-  err && console.log( 'Error: ', + err )
-  // res && scrape( res )
-  res && console.log( res )
-})
-
+//
+// const request = require( 'request' )
+// const url = 'https://my.pgp-hms.org/public_genetic_data'
+//
+// const cheerio = require('cheerio')
+//
+//
+// request( url, ( err, res ) => {
+//   err && console.log( 'Error: ', + err )
+//   res && scrape( res )
+//   // res && console.log( res )
+// })
+//
 function scrape( res ) {
-  $ = cheerio.load( res )
-  console.log( $('[data-summarize-as="participant"]').text() )
+  $ = cheerio.load( res.body )
+  console.log( $('td[data-summarize-as="participant"]') )
 }
+
+
+const fs = require( 'fs' )
+const path = require( 'path-resolve' )
+fs.readFile( './page-source.html', 'utf-8', function(err, data) {
+  err && console.log(err)
+  data && scrape( data )
+})
